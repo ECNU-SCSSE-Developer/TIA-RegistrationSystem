bf70d8357f6e2adf423c36c7042141e686869872
IwAntACqaqqaq <494812665@qq.com>
2018-10-18 21:28:22 +0800

    队伍界面

---
+++ miniprogram/pages/memberlist/memberlist.js
@@ -0,0 +1,83 @@
+// pages/memberlist/memberlist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    team: {
+      "data": [{
+        "id": 0,
+        "name": "卢宇博你是狗吧",
+        "major": "软件工程",
+        "phonenumber": "123"
+      }, {
+        "id": 1,
+          "name": "我儿子贺慈硕",
+          "major": "计科",
+          "phonenumber": "456"
+      }, {
+        "id": 2,
+          "name": "老孙",
+          "major": "软件",
+          "phonenumber": "789"
+      }]
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ miniprogram/pages/myteam/myteam.js
@@ -0,0 +1,84 @@
+// pages/myteam/myteam.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    team: {
+      "data": [{
+        "id": 0,
+        "name": "scsse",
+        "number": "待招3人",
+        "match": "智慧校园"
+      }, {
+        "id": 1,
+        "name": "coder",
+        "number": "待招5人",
+        "match": "科创培育项目"
+      }, {
+        "id": 2,
+        "name": "嘻哈小组",
+        "number": "待招1人",
+        "match": "智慧校园"
+      }]
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
