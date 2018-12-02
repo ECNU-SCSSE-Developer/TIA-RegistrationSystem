427dc1000bece9521ebb76b88ed576dc5335dda1
Andrew <ybdong374@126.com>
2018-12-03 03:26:36 +0800

    mine

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -92,7 +92,70 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
+    var that = this;
+    wx.login({
+      success: function (res) {
+        code: res.code
+      }
+    })
 
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    let studentId = wx.getStorageSync("studentId")
+    if (studentId) {
+      wx.request({
+        url: 'https://scsse.me/tia/user',
+        data: {
+          studentId: wx.getStorageSync('studentId')
+        },
+        method: 'GET',
+        header: {
+          'content-type': 'application/json;charset=utf-8',
+          'sessionid': wx.getStorageSync('sessionid')
+        },
+        success: function (res) {
+          console.log('personal info: ' + res.data);
+          that.setData({
+            man: res.data,
+          })
+        },
+        fail: function (res) {
+          console.log("Sorry,please try again!")
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
