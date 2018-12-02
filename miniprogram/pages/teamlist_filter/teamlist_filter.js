e79ab732bddb04afc4bfc6e94540964774730d46
IwAntACqaqqaq <494812665@qq.com>
2018-12-03 04:17:05 +0800

    filter功能实现

--- miniprogram/pages/teamlist_filter/teamlist_filter.js
+++ miniprogram/pages/teamlist_filter/teamlist_filter.js
@@ -4,8 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    caseList: [
-      {
+    caseList: [{
         message: 'foo'
       },
       {
@@ -13,75 +12,119 @@ Page({
       }
     ],
     dropDownMenuTitle: ['条件筛选'],
-    dropDownMenuFourthData: [{ id: 1, title: '智能排序' }, { id: 2, title: '发布时间' }, { id: 3, title: '距离优先' }],//排序数据
-    dropDownMenuFirstData: [
-      {
-        id: 1, title: '按比赛',
-        childModel: [
-          { id: '11', title: '智慧校园' },
-          { id: '12', title: '上海市“气象+”大数据' },
-          { id: '13', title: '“互联网+”创新创业大赛' }]
+    dropDownMenuFirstData: [{
+        id: 1,
+        title: '按比赛',
+        childModel: []
       },
       {
-        id: 2, title: '按招募职位',
-        childModel: [
-          { id: '21', title: 'web前端开发' },
-          { id: '22', title: 'Android开发' },
-          { id: '23', title: '小程序前端开发' },
-          { id: '24', title: 'java后端开发' }]
+        id: 2,
+        title: '按招募职位',
+        childModel: []
       },
-
+      {
+        id: 3,
+        title: '显示全部',
+        childModel: []
+      },
     ],
-    dropDownMenuSecondData: [
-      { id: 1, title: '标签11' },
-      { id: 2, title: '标签12' }],
-    dropDownMenuThirdData: [
-      { id: 1, title: '标签11' },
-      { id: 2, title: '标签12' }],
-
-    team: {
-      "data": [{
-        "id": 0,
-        "name": "scsse",
-        "number": "待招3人",
-        "match": "智慧校园"
-      }, {
-        "id": 1,
-        "name": "coder",
-        "number": "待招5人",
-        "match": "科创培育项目"
-      }, {
-        "id": 2,
-        "name": "嘻哈小组",
-        "number": "待招1人",
-        "match": "智慧校园"
-      }]
-    },
-    team:{}
+    shaiOrNot: 0,
+    team: {},
+    match: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
+    if (that.data.shaiOrNot == 0) {
+      wx.request({
+        url: 'https://scsse.me/tia/recruit',
+        data: {
+          "selectAll": 1
+        },
+        method: 'GET',
+        header: {
+          'content-type': 'application/json;charset=utf-8',
+          'sessionid': wx.getStorageSync('sessionid')
+        },
+        success: function(res) {
+          console.log('personal info: ' + res.data);
+          that.setData({
+            team: res.data,
+          })
+        },
+        fail: function(res) {
+          console.log("Sorry,please try again!")
+        }
+      })
+    }
     wx.request({
-      url: 'https://scsse.me/tia/recruit',
+      url: 'https://scsse.me/tia/match',
       data: {
-        "selectAll" : 1
+        "selectAll": 1
       },
       method: 'GET',
       header: {
         'content-type': 'application/json;charset=utf-8',
         'sessionid': wx.getStorageSync('sessionid')
       },
-      success: function (res) {
-        console.log('personal info: ' + res.data);
+      success: function(res) {
+        console.log('获得比赛列表：' + res.data);
         that.setData({
-          team: res.data,
+          match: res.data,
         })
+        var j = 11;
+        var index = 0;
+        for (var i in res.data) {
+          var item = "dropDownMenuFirstData[0].childModel[" + index + "]";
+          console.info(i)
+          that.setData({
+            [item]: {
+              id: j,
+              title: res.data[i].matchName
+            }
+          })
+          console.info(that.data.dropDownMenuFirstData[0].childModel[index])
+          j++
+          index++
+        }
       },
-      fail: function (res) {
+      fail: function(res) {
+        console.log("Sorry,please try again!")
+      }
+    })
+    wx.request({
+      url: 'https://scsse.me/tia/allType',
+      data: {},
+      method: 'GET',
+      header: {
+        'content-type': 'application/json;charset=utf-8',
+        'sessionid': wx.getStorageSync('sessionid')
+      },
+      success: function(res) {
+        console.log('获得招聘类型列表：' + res.data);
+        that.setData({
+          recruitType: res.data,
+        })
+        var j = 21;
+        var index = 0;
+        for (var i in res.data) {
+          var item = "dropDownMenuFirstData[1].childModel[" + index + "]";
+          console.info(i)
+          that.setData({
+            [item]: {
+              id: j,
+              title: res.data[i]
+            }
+          })
+
+          j++
+          index++
+        }
+      },
+      fail: function(res) {
         console.log("Sorry,please try again!")
       }
     })
@@ -91,64 +134,149 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  selectedFourth: function (e) {
+  selectedFourth: function(e) {
+    var that = this;
     console.log("选中第" + e.detail.index + "个标签，选中的id：" + e.detail.selectedId + "；选中的内容：" + e.detail.selectedTitle);
+
+    if (e.detail.selectedId >= 20) {
+      var index = parseInt(e.detail.selectedId) - 21;
+      console.info("1")
+      wx.request({
+        url: 'https://scsse.me/tia/typedRecruit',
+        data: {
+          "recruitType": that.data.dropDownMenuFirstData[1].childModel[index].title
+        },
+        method: 'GET',
+        header: {
+          'content-type': 'application/json;charset=utf-8',
+          'sessionid': wx.getStorageSync('sessionid')
+        },
+        success: function(res) {
+          console.log('personal info: ' + res.data);
+          that.setData({
+            team: res.data,
+            shaiOrNot: 1
+          })
+          wx.navigateTo({
+            url: '../teamlist_filter/teamlist_filter',
+          })
+
+        },
+        fail: function(res) {
+          console.log("Sorry,please try again!")
+        }
+      })
+    } else if (e.detail.selectedId == 3) {
+      console.info("2")
+      that.data.shaiOrNot == 0;
+      wx.request({
+        url: 'https://scsse.me/tia/recruit',
+        data: {
+          "selectAll": 1
+        },
+        method: 'GET',
+        header: {
+          'content-type': 'application/json;charset=utf-8',
+          'sessionid': wx.getStorageSync('sessionid')
+        },
+        success: function (res) {
+          console.log('personal info: ' + res.data);
+          that.setData({
+            team: res.data,
+          })
+        },
+        fail: function (res) {
+          console.log("Sorry,please try again!")
+        }
+      })
+      wx.navigateTo({
+        url: '../teamlist_filter/teamlist_filter',
+      })
+    } else {
+      console.info("3")
+      var index = parseInt(e.detail.selectedId) - 11;
+      wx.request({
+        url: 'https://scsse.me/tia/recruit/match',
+        data: {
+          "matchName": e.detail.selectedTitle
+        },
+        method: 'GET',
+        header: {
+          'content-type': 'application/json;charset=utf-8',
+          'sessionid': wx.getStorageSync('sessionid')
+        },
+        success: function(res) {
+          console.log('personal info: ' + res.data);
+          that.setData({
+            team: res.data,
+            shaiOrNot: 1
+          })
+          wx.navigateTo({
+            url: '../teamlist_filter/teamlist_filter',
+          })
+
+        },
+        fail: function(res) {
+          console.log("Sorry,please try again!")
+        }
+      })
+    }
   },
-  showDialog: function (e) {
+  showDialog: function(e) {
 
   },
   //取消事件
-  _cancelEvent: function (e) {
+  _cancelEvent: function(e) {
     console.log('你点击了取消');
     this.dialog.hideDialog();
   },
   //确认事件
-  _confirmEvent: function (e) {
+  _confirmEvent: function(e) {
     console.log('你点击了确定');
     this.dialog.hideDialog();
   }
