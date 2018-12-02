a7c7f8f42d2c9e45f26aebf8ef95e13649eaa9ad
Andrew <ybdong374@126.com>
2018-12-03 05:34:59 +0800

    teamform修改

--- miniprogram/pages/teamform/teamform.js
+++ miniprogram/pages/teamform/teamform.js
@@ -1,4 +1,4 @@
-var position = 0;
+var position = "";
 var index = 0;
 
 Page({
@@ -6,43 +6,38 @@ Page({
     array: ['请选择招募位置', '前端', '后端'],
     index_position: 0,
     objectArray: [{
-      id: 0,
-      name: '前端'
-    },
-    {
-      id: 1,
-      name: '后端'
-    },
+        id: 0,
+        name: '前端'
+      },
+      {
+        id: 1,
+        name: '后端'
+      },
     ],
   },
- bindCasePickerChange_position: function (e) {
-  if(e.detail.value == 2) {
-  position = this.data.objectArray[2].name;
-  index = this.data.objectArray[2].id;
-}
-if (e.detail.value == 1) {
-  position = this.data.objectArray[1].name;
-  index = this.data.objectArray[1].id;
-}
-if (e.detail.value == 0) {
-  position = this.data.objectArray[0].name;
-  index = this.data.objectArray[0].id;
-}
-this.setData({
-  index_position: e.detail.value
-})
-},
+  bindCasePickerChange_position: function(e) {
+    if (e.detail.value == 2) {
+      position = this.data.objectArray[1].name;
+      index = this.data.objectArray[1].id;
+    }
+    if (e.detail.value == 1) {
+      position = this.data.objectArray[0].name;
+      index = this.data.objectArray[0].id;
+    }
+    this.setData({
+      index_position: e.detail.value
+    })
+  },
 
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     var that = this;
     let {
       name,
       num,
       description,
       requirements,
-      position
     } = e.detail.value;
-
+    let myposition = position;
     if (e.detail.value.name.length == 0) {
       wx.showModal({
         content: "请填写项目名称！",
@@ -50,33 +45,28 @@ this.setData({
         confirmColor: "#557d8a",
         confirmText: "好的",
       });
-    }
-    else if (e.detail.value.num.length == 0) {
+    } else if (e.detail.value.num.length == 0) {
       wx.showModal({
         content: "请填写学号信息！",
         showCancel: false,
         confirmColor: "#557d8a",
         confirmText: "好的",
       });
-    }
-      else if (e.myposition == 0) {
-          wx.showModal({
-            content: "请选择招募位置！",
-            showCancel: false,
-            confirmColor: "#557d8a",
-            confirmText: "好的",
-          })
-         }
-      
-    else if (e.detail.value.description.length == 0) {
+    } else if (position == "") {
+      wx.showModal({
+        content: "请选择招募位置！",
+        showCancel: false,
+        confirmColor: "#557d8a",
+        confirmText: "好的",
+      })
+    } else if (e.detail.value.description.length == 0) {
       wx.showModal({
         content: "请填写项目简介！",
         showCancel: false,
         confirmColor: "#557d8a",
         confirmText: "好的",
       });
-    }
-    else if (e.detail.value.requirements.length == 0) {
+    } else if (e.detail.value.requirements.length == 0) {
       wx.showModal({
         content: "请填写招募要求！",
         showCancel: false,
@@ -96,10 +86,10 @@ this.setData({
           "recruitName": name,
           "recruitDescription": description,
           "recruitRequirements": requirements,
-          "recruitPosition": positions,
-          "matchId":"123"
+          "recruitPosition": myposition,
+          "matchId": "123"
         },
-        success: function () {
+        success: function() {
           console.info("userID:" + e.detail.value.num);
           wx.setStorageSync("userId", e.detail.value.num);
           wx.showToast({
@@ -107,68 +97,68 @@ this.setData({
             icon: 'success',
             duration: 1000
           });
-          setTimeout(function () {
+          setTimeout(function() {
             wx.switchTab({
               url: "/pages/matchlist"
             });
           }, 1000)
         },
-        fail: function () {
+        fail: function() {
           console.log("please try again");
         }
       })
     }
   },
   /**
-    * 生命周期函数--监听页面加载
-    */
-  onLoad: function (options) {
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
 
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () { },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
